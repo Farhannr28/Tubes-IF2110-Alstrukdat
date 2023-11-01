cd5a25cfa4a099c24224c833ed9d979439a27a0e
Muhammad Sulthan Mazaya <msulthanmazaya@gmail.com>
2023-11-01 20:33:31 +0700

    feat: add greetings

--- src/main.c
+++ src/main.c
@@ -1,3 +1,22 @@
+#include <stdio.h>
 #include <wordmachine.h>
 
-int main() { return 0; }
+void printGreeting() {
+  printf(".______    __    __  .______      .______    __  .______      \n"
+         "|   _  \\  |  |  |  | |   _  \\     |   _  \\  |  | |   _  \\     \n"
+         "|  |_)  | |  |  |  | |  |_)  |    |  |_)  | |  | |  |_)  |    \n"
+         "|   _  <  |  |  |  | |      /     |   _  <  |  | |      /     \n"
+         "|  |_)  | |  `--'  | |  |\\  \\----.|  |_)  | |  | |  |\\  \\----.\n"
+         "|______/   \\______/  | _| `._____||______/  |__| | _| `._____|  \n"
+         "Selamat datang di BurBir. \n"
+         "Aplikasi untuk studi kualitatif mengenai perilaku manusia dengan "
+         "menggunakan metode (pengambilan data berupa) Focused Group "
+         "Discussion kedua di zamannya.\n"
+         "Silahkan masukan folder konfigurasi untuk dimuat: config-1;\n"
+         "File konfigurasi berhasil dimuat! Selamat berkicau!\n");
+}
+
+int main() {
+  printGreeting();
+  return 0;
+}
