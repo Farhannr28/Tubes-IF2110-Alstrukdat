98565b557298c8db01f17183a392a6f4461c15ba
Muhammad Sulthan Mazaya <msulthanmazaya@gmail.com>
2023-11-13 12:19:16 +0700

    feat: add inisialisasi dan perintah

--- lib/user.h
+++
@@ -1,9 +0,0 @@
-#ifndef __USER_H__
-#define __USER_H__
-
-#include "boolean.h"
-#include "stdio.h"
-
-void RegisterUser() { printf("Lagi register nih\n"); }
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,3 +1,40 @@
 #include <stdio.h>
+#include <wordmachine.h>
 
-int main() { return 0; }
+void greetings() {
+  Word configFile;
+  printf(".______    __    __  .______      .______    __  .______      \n"
+         "|   _  \\  |  |  |  | |   _  \\     |   _  \\  |  | |   _  \\     \n"
+         "|  |_)  | |  |  |  | |  |_)  |    |  |_)  | |  | |  |_)  |    \n"
+         "|   _  <  |  |  |  | |      /     |   _  <  |  | |      /     \n"
+         "|  |_)  | |  `--'  | |  |\\  \\----.|  |_)  | |  | |  |\\  \\----.\n"
+         "|______/   \\______/  | _| `._____||______/  |__| | _| `._____|  \n"
+         "Selamat datang di BurBir. \n"
+         "Aplikasi untuk studi kualitatif mengenai perilaku manusia dengan "
+         "menggunakan metode (pengambilan data berupa) Focused Group "
+         "Discussion kedua di zamannya.\n"
+         "Silahkan masukan folder konfigurasi untuk dimuat: ");
+  GetWord(&configFile);
+  // TODO: load config
+  printf("File konfigurasi berhasil dimuat! Selamat berkicau!\n");
+}
+
+Word perintah;
+
+void getPerintah() {
+  printf(">> ");
+  GetWord(&perintah);
+}
+
+int main() {
+  greetings();
+  while (true) {
+    getPerintah();
+    PrintWord(perintah);
+    printf("\n");
+    if (WordCmp(perintah, "KELUAR")) {
+      break;
+    }
+  }
+  return 0;
+}
