19c0390eb588bf263b37ba9c019d79f346c10fd8
Muhammad Sulthan Mazaya <msulthanmazaya@gmail.com>
2023-11-15 01:57:21 +0700

    chore: organize a bit

--- src/main.c
+++ src/main.c
@@ -39,57 +39,73 @@ void PromptUser(const char *c, Word *w) {
   GetWord(w);
 }
 
+void DoKeluar() {
+  if (IsUserValid(currentUser)) {
+    printf("Anda berhasil logout. Sampai jumpa di pertemuan berikutnya!\n");
+    InvalidateUser(&currentUser);
+  } else {
+    printf("Anda belum login! Masuk terlebih dahulu untuk menikmati layanan "
+           "BurBir.\n");
+  }
+}
+
+void DoDaftar() {
+  Word nama, password;
+  while (true) {
+    PromptUser("Masukkan nama:\n", &nama);
+    if (UsernameTaken(listUser, nama)) {
+      printf("Wah, sayang sekali nama tersebut telah diambil.\n");
+    } else {
+      break;
+    }
+  }
+  PromptUser("Masukkan kata sandi:\n", &password);
+  Pengguna newUser;
+  CreatePengguna(&newUser, nama, password);
+  insertFirstListPengguna(&listUser, newUser);
+  printf("Pengguna telah berhasil terdaftar. Masuk untuk menikmati "
+         "fitur-fitur BurBir.\n");
+}
+
+void DoMasuk() {
+  Word nama, password;
+  while (true) {
+    PromptUser("Masukkan nama:\n", &nama);
+    if (UsernameTaken(listUser, nama)) {
+      break;
+    } else {
+      printf("Wah, nama yang Anda cari tidak ada. Masukkan nama lain!\n");
+    }
+  }
+  while (true) {
+    PromptUser("Masukkan kata sandi:\n", &password);
+    if (UserAndPasswordMatch(listUser, nama, password)) {
+      CreatePengguna(&currentUser, nama, password);
+      break;
+    } else {
+      printf("Wah, kata sandi yang Anda masukkan belum tepat. Periksa "
+             "kembali kata sandi Anda!\n");
+    }
+  }
+  printf("Anda telah berhasil masuk dengan nama pengguna Tuan Bri. Mari "
+         "menjelajahi BurBir bersama Ande-Ande Lumut!\n");
+}
+
+void DoTutupProgram() {
+  isStop = true;
+  printf("Anda telah keluar dari program BurBir.\n Sampai jumpa di "
+         "penjelajahan berikutnya.\n");
+}
+
 void DoPerintah() {
   if (WordCmp(perintah, "KELUAR")) {
-    if (IsUserValid(currentUser)) {
-      printf("Anda berhasil logout. Sampai jumpa di pertemuan berikutnya!\n");
-      InvalidateUser(&currentUser);
-    } else {
-      printf("Anda belum login! Masuk terlebih dahulu untuk menikmati layanan "
-             "BurBir.\n");
-    }
+    DoKeluar();
   } else if (WordCmp(perintah, "DAFTAR")) {
-    Word nama, password;
-    while (true) {
-      PromptUser("Masukkan nama:\n", &nama);
-      if (UsernameTaken(listUser, nama)) {
-        printf("Wah, sayang sekali nama tersebut telah diambil.\n");
-      } else {
-        break;
-      }
-    }
-    PromptUser("Masukkan kata sandi:\n", &password);
-    Pengguna newUser;
-    CreatePengguna(&newUser, nama, password);
-    insertFirstListPengguna(&listUser, newUser);
-    printf("Pengguna telah berhasil terdaftar. Masuk untuk menikmati "
-           "fitur-fitur BurBir.\n");
+    DoDaftar();
   } else if (WordCmp(perintah, "MASUK")) {
-    Word nama, password;
-    while (true) {
-      PromptUser("Masukkan nama:\n", &nama);
-      if (UsernameTaken(listUser, nama)) {
-        break;
-      } else {
-        printf("Wah, nama yang Anda cari tidak ada. Masukkan nama lain!\n");
-      }
-    }
-    while (true) {
-      PromptUser("Masukkan kata sandi:\n", &password);
-      if (UserAndPasswordMatch(listUser, nama, password)) {
-        CreatePengguna(&currentUser, nama, password);
-        break;
-      } else {
-        printf("Wah, kata sandi yang Anda masukkan belum tepat. Periksa "
-               "kembali kata sandi Anda!\n");
-      }
-    }
-    printf("Anda telah berhasil masuk dengan nama pengguna Tuan Bri. Mari "
-           "menjelajahi BurBir bersama Ande-Ande Lumut!\n");
+    DoMasuk();
   } else if (WordCmp(perintah, "TUTUP_PROGRAM")) {
-    isStop = true;
-    printf("Anda telah keluar dari program BurBir.\n Sampai jumpa di "
-           "penjelajahan berikutnya.\n");
+    DoTutupProgram();
   } else {
     printf("Perintah tidak valid!\n");
   }
