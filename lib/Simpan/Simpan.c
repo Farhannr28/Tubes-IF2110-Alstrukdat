93939d8e1d974268d15a37d9b908964e27dcbde9
Razark-Y <wilsonyusda@gmail.com>
2023-11-23 22:56:06 +0700

    Refactor: Change concat function

--- lib/Simpan/Simpan.c
+++ lib/Simpan/Simpan.c
@@ -9,9 +9,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+void str_copy(char *dest, const char *src, size_t dest_size) {
+    size_t i;
+    for (i = 0; src[i] != '\0' && i < dest_size - 1; i++) {
+        dest[i] = src[i];
+    }
+    dest[i] = '\0'; 
+}
+
+void str_concat(char *dest, const char *src, size_t dest_size) {
+    size_t i = 0;
+    while (dest[i] != '\0' && i < dest_size) {
+        i++;
+    }
+
+    if (i < dest_size - 1) {
+        size_t j;
+        for (j = 0; src[j] != '\0' && i + j < dest_size - 1; j++) {
+            dest[i + j] = src[j]; 
+        }
+        dest[i + j] = '\0'; 
+    }
+}
+
+
 void SIMPANUTAS(ListLinearUtas ListUtas, const char *folderName) {
     char dirPath[1024];
-    snprintf(dirPath, sizeof(dirPath), "%s", folderName);
+    str_copy(dirPath, folderName, sizeof(dirPath));
     struct stat st = {0};
     if (stat(dirPath, &st) == -1) {
         if (mkdir(dirPath) != 0) {
@@ -21,8 +45,9 @@ void SIMPANUTAS(ListLinearUtas ListUtas, const char *folderName) {
     }
 
     char filePath[1024];
-    snprintf(filePath, sizeof(filePath), "%s/utas.config", dirPath);
-
+    str_copy(filePath, dirPath, sizeof(filePath));
+    str_concat(filePath, "/utas.config", sizeof(filePath));
+    printf("%s\n",filePath);
     FILE *file = fopen(filePath, "w");
     if (file == NULL) {
         fprintf(stderr, "Unable to open file %s for writing.\n", filePath);
