abbc20d00a9b735fc3fd492fb6b6d7f219db3d45
Razark-Y <wilsonyusda@gmail.com>
2023-11-24 00:18:19 +0700

    feat: Added Utasan Driver

--- lib/Utasan/utasan_driver.c
+++ lib/Utasan/utasan_driver.c
@@ -8,9 +8,130 @@
 #include "../Pengguna/pengguna.h"
 #include <stdio.h>
 #include "Utasan.h"
-#include "../ListLinier/listlinier.h"
+#include "../Sederhana/ctime.h"
+#include "../Sederhana/datetime.h"
 int main(){
     //Set up kondisi
-    crea
-    return 0;
+    ListLinearUtas ListUtas;
+    int type;
+    DATETIME D = GetCurrentDateTime();
+    Word UtasanBaru;
+    Utasan U;
+    Word UtasanInput;
+    Word Penulis;
+    Word PenulisOutput;
+    ListStatik User;
+    ListDin ListKicau;
+    Kicauan kicauan;
+    Word Utama;
+    Word Text2;
+    CreateListLinearUtas(&ListUtas);
+    printf("Initialized test cases :\n");
+    printf("Masukkan Utasan untuk driver test:\n");
+    GetWord(&UtasanInput);
+    printf("Masukkan PENULIS untuk driver test:\n");
+    GetWord(&Penulis);
+    CreateUtas(&U,23,23,UtasanInput,Penulis,D);
+    insertLastListLinearUtas(&ListUtas,U);
+    AddressUtas p = ListUtas;
+    int IndexUtas;
+    int indexmax;
+    Paragraph* paragraph = p->info.TextList;
+    printf("Kondisi yang ingin di tes\n");
+    scanf("%d",&type);
+    switch (type)
+    {
+        case 1:
+        printf("Drive test untuk insertLastParagraph\n");
+        printf("Masukkan kicauan sambungan:\n");
+        GetWord(&Text2);
+        insertLastParagraph(&U,Text2,D);
+        int index = 1;
+        printf("IDKICAU = %d ",U.IDKicauan);
+        printf("IDUTAS = %d  ",U.IDUtasan);
+        printf("\n");
+        printf("Penulis yaitu:\n");
+        PrintWord(Penulis);
+        printf("\n");
+        while(p!=NULL){
+            while( paragraph !=NULL){
+                printf("Ini untuk index %d\n",index);
+                printf("Pada jam:\n");
+                TulisDateTime(CurrTime(paragraph));
+                printf("\n");
+                printf("Dituliskan:");
+                PrintWord(KONTEN(paragraph));
+                printf("\n");
+                printf("\n");
+                paragraph =  paragraph-> next;
+                index ++;
+            }
+            p = NEXT(p);
+        }
+        break;
+
+        case 2:
+        printf("Drive test untuk CreateUtas\n");
+        index = 1;
+        printf("IDKICAU = %d ",U.IDKicauan);
+        printf("IDUTAS = %d  ",U.IDUtasan);
+        printf("\n");
+        printf("Penulis yaitu:\n");
+        PrintWord(Penulis);
+        printf("\n");
+        while(p!=NULL){
+            while( paragraph !=NULL){
+                printf("Pada jam:\n");
+                TulisDateTime(CurrTime(paragraph));
+                printf("\n");
+                printf("Dituliskan:");
+                PrintWord(KONTEN(paragraph));
+                printf("\n");
+                printf("\n");
+                paragraph =  paragraph-> next;
+                index ++;
+            }
+            p = NEXT(p);
+        }
+        break;
+        case 3:
+            printf("Drive test untuk deleteParagraphAtPosition\n");
+            printf("Masukkan kicauan sambungan:\n");
+            GetWord(&Text2);
+            insertLastParagraph(&U,Text2,D);
+            index = 1;
+            printf("Masukkan input index yang ingin dihapus, pastikkan valid:\n");
+            // scanf("%d",&IndexUtas);
+            deleteParagraphAtPosition(&ListUtas,23,1);
+            paragraph = p->info.TextList;
+            while(p!=NULL){
+                while( paragraph !=NULL){
+                    printf("Ini untuk index %d\n",index);
+                    printf("Pada jam:\n");
+                    TulisDateTime(CurrTime(paragraph));
+                    printf("\n");
+                    printf("Dituliskan:");
+                    PrintWord(KONTEN(paragraph));
+                    printf("\n");
+                    printf("\n");
+                    paragraph =  paragraph-> next;
+                    index ++;
+                }
+            p = NEXT(p);
+            break;
+        }
+        case 4:
+            printf("Driver test untuk GetUtasanIndex\n");
+            indexmax = GetUtasanIndex(ListUtas,23);
+            printf("Index max Awal:%d\n",indexmax);
+            printf("Tambah sambungan text:\n");
+            GetWord(&Text2);
+            insertLastParagraph(&U,Text2,D);
+            indexmax = GetUtasanIndex(ListUtas,23);
+            printf("Index max Akhir:%d\n",indexmax);
+        case 5:
+            printf("Driver test untuk indexOfListLinearUtas\n");
+            indexmax = indexOfListLinearUtas(ListUtas,23,&PenulisOutput);
+            printf("Index dalam List (IDX_UNDEF jika tidak ada):%d",indexmax);
+}
 }
