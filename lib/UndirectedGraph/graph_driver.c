c121023f5af9060c6dff07847601148f7d87e61b
AlthariqFairuz <althariqfairuz273@gmail.com>
2023-11-24 15:55:21 +0700

    feat: add driver graph

--- lib/UndirectedGraph/graph_driver.c
+++ lib/UndirectedGraph/graph_driver.c
@@ -1,32 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "graph.h"
 
 int main()
 {
-    User user1;
-    User user2;
-    User user3;
-    Graph graph;
-    createGraph(&graph, 20);
-    createUser(&user1, "user1", 100);
-    createUser(&user2, "user2", 200);
-    createUser(&user3, "user3", 300);
-
-    addTeman(&graph, INDEX_USER(user1), INDEX_USER(user2));
-    addTeman(&graph, INDEX_USER(user1), INDEX_USER(user3));
-    hapusTeman(&graph, INDEX_USER(user1), INDEX_USER(user2));
-
-    printTeman(graph, user1, INDEX_USER(user1));
-    printf("====================================\n");
-    printTeman(graph, user2, INDEX_USER(user2));
-    printf("====================================\n");
-    boolean tes = isNoTeman(graph, INDEX_USER(user1));
-    printf("%d\n", tes);
-    printf("====================================\n");
-    tes = isTeman(graph, INDEX_USER(user1), INDEX_USER(user3));
-    printf("%d\n", tes);
-
-    return 0;
+    int input;
+    scanf("Masukkan test case: %d", &input);
+    switch (input)
+    { 
+    case 1:
+        printf("Test case 1\n");
+        Graph G;
+        CreateEmptyGraph(&G);
+        print("IsNoTeman User 1: ", isNoTeman(G,1));
+        print("IsTeman User 1 with User 2: ", isTeman(G,1,2));
+        print("Jumlah teman user 1: %d:",jumlahTeman(G,1));
+        print("Jumlah teman user 2: %d:",jumlahTeman(G,2));
+        printf("Menambahkan teman user 1 dengan user 2\n");
+        sendRequest(&G,1,2);
+        break;
+    }
 }
